1e371fb56b49c320640a8966d3ae0860ca8966d4
BenFaruna <benjaminfaruna@gmail.com>
2023-02-02 23:39:07 +0100

    Completed filter_datum function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""module for logging"""
+import logging
+import re
+
+from typing import List
+
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str):
+    """function returns the log message obfuscated"""
+    pattern = f"(?<={separator})(" + "|".join(fields) + f")=(.+?);"
+    message = re.sub(pattern, f"{separator}\\1={redaction};", message)
+    return re.sub(f"{separator}{{2,}}", f"{separator}", message)

881c667c02aad7b8774a6f5078abcee36a8b7e72
BenFaruna <benjaminfaruna@gmail.com>
2023-02-03 14:41:42 +0100

    completed password encryption and checking

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""module for encrypting password"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """creates a password hash from the password argument"""
+    salt = bcrypt.gensalt()
+    hashpw = bcrypt.hashpw(password.encode(), salt)
+    return hashpw
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """checks password if it matches the hashed password"""
+    validity = bcrypt.checkpw(password.encode(), hashed_password)
+    return validity
